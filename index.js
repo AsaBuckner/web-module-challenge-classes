306ed7d3d719fb8c5cbd7bd8eb9f726f7544f1f1
Dupigny-Leigh Leigh <asabuckner19@gmail.com>
2022-08-03 01:03:06 -0700

    Attempt #1 (Drive function not completed)

--- index.js
+++ index.js
@@ -44,7 +44,21 @@ class Airplane {
 */
 
 class Person {
-  
+  constructor (name, age){
+    this.name = name
+    this.age = age
+    this.stomach = []
+  }
+  eat(){
+    if(this.stomach.length < 10){
+      this.stomach.push(food)}
+    }
+  poop(){
+    this.stomach = []
+  }
+  toString(){
+    return `${this.name}, ${this.age}`
+  }
 }
 
 /*
@@ -62,7 +76,19 @@ class Person {
 */
 
 class Car {
-  
+  constructor (model, milesPerGallon){
+    this.model= model
+    this.milesPerGallon = milesPerGallon
+    this.tank = 0;
+    this.odometer = 0;
+  }
+  fill(gallons){
+    this.tank = gallons + this.tank
+  }
+  drive(distance){
+    this.odometer = this.odometer + distance;
+    
+  }
 }
 
 /*
@@ -79,7 +105,14 @@ class Car {
 */
 
 class Lambdasian {
-  
+  constructor (object){
+this.name = object.name
+this.age = object.age
+this.location = object.location
+  }
+speak(){
+  return `Hello my name is ${this.name}, I am from ${this.location}`
+}
 }
 
 /*
@@ -97,8 +130,19 @@ class Lambdasian {
         + `grade` receives a `student` object and a `subject` string as arguments and returns '{student.name} receives a perfect score on {subject}'
 */
 
-class Instructor {
-
+class Instructor extends Lambdasian{
+  constructor (object){
+  super(object);
+  this.specialty = object.specialty;
+  this.favLanguage = object.favLanguage;
+  this.catchPhrase = object.catchPhrase;
+}
+demo(subject){
+  return `Today we are learning about ${subject}`
+}
+grade(student, subject){
+ return `${student.name} receives a perfect score on ${subject}`
+}
 }
 
 /*
@@ -117,8 +161,22 @@ class Instructor {
         + `sprintChallenge` similar to PRAssignment but returns `student.name has begun sprint challenge on {subject}`
 */
 
-class Student {
-   
+class Student extends Lambdasian {
+   constructor(object){
+super(object)
+this.previousBackground = object.previousBackground
+this.className = object.className
+this.favSubjects = object.favSubjects
+   }
+   listSubjects(){
+    return toString(this.favSubjects)
+   }
+   PRAssignment(subject){
+return `${student.name} has submitted a PR for ${subject}`
+   }
+   sprintChallenge(subject){
+    returns `${this.student.name} has begun sprint challenge on ${this.subject}`
+   }
 }
 
 /*
@@ -135,8 +193,18 @@ class Student {
         + `debugsCode` a method that takes in a student object and a subject and returns `{name} debugs {student.name}'s code on {subject}`
 */
 
-class ProjectManager {
-   
+class ProjectManager extends Instructor{
+  constructor(object){
+  super(object)
+this.gradClassName = object.gradClassName
+this.favInstructor = object.favInstructor
+  }
+standup(channel){
+  `${this.name} announces to ${this.channel}, @channel standy times!`
+}
+debugsCode(student,subject){
+  `${this.name} debugs ${student.name}'s code on ${subject}`
+}
 }
 
 /*
